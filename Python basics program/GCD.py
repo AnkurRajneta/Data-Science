8d07709a4fa7e16a8c639b1e08344e0ae72ea920
AnkurRajneta <ankur_207003@saitm.org>
2022-09-21 19:02:02 +0530

    ☑️

--- Python basics program/GCD.py
+++
@@ -1,7 +0,0 @@
-a=int(input())
-b=int(input())
-
-
-while a!=0:
-    a,b=b%a,a
-print("The GCD :",b)
--- Python basics program/Pattern.py
+++
@@ -1,6 +0,0 @@
-n=4
-for i in range(1,n+1):
-    c=chr(ord('A')+i-1)
-    s=(c+" ")*i
-    print(s)
-    
